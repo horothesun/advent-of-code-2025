39617600c9662a7473f76483c14f87ec1a7b927f
horothesun <horothesun@gmail.com>
2025-12-01 23:29:43 +0000

    Day 1: part 1

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,6 +1,50 @@
-import cats.derived.*
+import cats.data.NonEmptyList
+import cats.parse.Numbers.nonNegativeIntString
+import cats.parse.Parser
+import cats.parse.Parser.{char, Error}
 import cats.syntax.all.*
+import scala.annotation.tailrec
 
 object Day01:
 
-  def day01: Int = 42
+  def modSubtraction(lhs: Int, rhs: Int, mod: Int): Int =
+    @tailrec def aux(i: Int): Int = if i >= 0 then i else aux(i + mod)
+    val sub = lhs - rhs
+    if sub >= 0 then sub else aux(sub)
+
+  opaque type Pos = Int
+
+  extension (p: Pos)
+    def left(n: Int): Pos = modSubtraction(lhs = p, rhs = n, mod = 100)
+    def right(n: Int): Pos = (p + n) % 100
+
+  object Pos:
+    def apply(n: Int): Pos = n
+    def zero: Pos = 0
+
+  enum Direction:
+    case Left, Right
+
+  object Direction:
+    def parser: Parser[Direction] = char('L').as(Left) | char('R').as(Right)
+
+  case class Rotation(d: Direction, n: Int)
+
+  object Rotation:
+    def parser: Parser[Rotation] =
+      (Direction.parser ~ nonNegativeIntString.mapFilter(_.toIntOption)).map(Rotation.apply)
+
+  def parseInput(rows: List[String]): Either[Error, List[Rotation]] = rows.traverse(Rotation.parser.parseAll)
+
+  def run(start: Pos, rotations: List[Rotation]): NonEmptyList[Pos] =
+    rotations
+      .foldLeft(NonEmptyList.one(start)) { case (acc @ NonEmptyList(p, tail), r) =>
+        (r.d match {
+          case Direction.Left  => p.left(r.n)
+          case Direction.Right => p.right(r.n)
+        }) :: acc
+      }
+      .reverse
+
+  def part1Solution(rows: List[String]): Either[Error, Long] =
+    parseInput(rows).map(rotations => run(start = Pos(50), rotations).count(_ == Pos.zero))
--- src/test/scala/Day01Suite.scala
+++ src/test/scala/Day01Suite.scala
@@ -1,14 +1,67 @@
 import Day01.*
 import Day01Suite.*
+import cats.data.NonEmptyList
+import cats.parse.Parser.Error
+import cats.syntax.all.*
 import munit.ScalaCheckSuite
 import org.scalacheck.Gen
 import org.scalacheck.Prop.*
 
 class Day01Suite extends ScalaCheckSuite:
 
-  test("day01 == 42"):
-    assertEquals(day01, 42)
+  test("small input parsed correctly"):
+    assertEquals(
+      parseInput(smallInput),
+      List(
+        Rotation(Direction.Left, 68),
+        Rotation(Direction.Left, 30),
+        Rotation(Direction.Right, 48),
+        Rotation(Direction.Left, 5),
+        Rotation(Direction.Right, 60),
+        Rotation(Direction.Left, 55),
+        Rotation(Direction.Left, 1),
+        Rotation(Direction.Left, 99),
+        Rotation(Direction.Right, 14),
+        Rotation(Direction.Left, 82)
+      ).asRight[Error]
+    )
+
+  test("big input parsed to something"):
+    assert(parseInput(bigInput).isRight)
+
+  property("modulo subtraction of positive numbers is always positive"):
+    forAll(Gen.posNum[Int], Gen.posNum[Int], Gen.posNum[Int])((lhs, rhs, mod) =>
+      assert(modSubtraction(lhs, rhs, mod) >= 0)
+    )
+
+  test("run small input rotations"):
+    assertEquals(
+      parseInput(smallInput).map(rotations => run(start = Pos(50), rotations)),
+      NonEmptyList
+        .of(
+          Pos(50),
+          Pos(82),
+          Pos(52),
+          Pos(0),
+          Pos(95),
+          Pos(55),
+          Pos(0),
+          Pos(99),
+          Pos(0),
+          Pos(14),
+          Pos(32)
+        )
+        .asRight[Error]
+    )
+
+  test("part 1 solution on small input is 3"):
+    assertEquals(part1Solution(smallInput), 3L.asRight[Error])
+
+  test("part 1 solution on big input is 1_007"):
+    assertEquals(part1Solution(bigInput), 1_007L.asRight[Error])
 
 object Day01Suite:
+
+  val smallInput: List[String] = List("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82")
 
   val bigInput: List[String] = getLinesFromFile("src/test/scala/day01_input.txt")
